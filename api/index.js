130822f121d7f0cbfa22af2a3e1e490ba980cf5a
weskin <not_wailmed@outlook.fr>
2023-10-22 18:02:54 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("conntected to MongoDB !");
+}).catch((error) => {
+    console.log("could not connect to MongoDB : ", error);
+});
 
 const app = express();
 
